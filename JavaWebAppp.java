3b7f63c359310e79392601f1c3e493b1f547881c
Aswinth123 <aswinth.k.cse26@psvpec.in>
2025-04-16 11:21:57 +0530

    Update JavaWebAppp.java for Azure Pipelines

--- JavaWebAppp.java
+++ JavaWebAppp.java
@@ -1,7 +1,7 @@
 import java.util.*;
-  public class JavaWebAppp{
-    public static void main(String[] args)
-    {
-      System.out.println("Welcome to the World");
-    }
+
+public class JavaWebAppp {
+  public static void main(String[] args) {
+    System.out.println("Welcome to the World");
   }
+}
