526d1924eeafea8492ce3ca493b58faff40dca5c
Aswinth123 <aswinth.k.cse26@psvpec.in>
2025-04-16 11:24:46 +0530

    Update JavaWebAppp.java for Azure Pipelines

--- JavaWebAppp.java
+++ JavaWebAppp.java
@@ -1,17 +1,6 @@
 import java.util.*;
-
 public class JavaWebAppp {
   public static void main(String[] args) {
     System.out.println("Welcome to the World");
   }
 }
-- task: Maven@4
-  inputs:
-    mavenPomFile: 'pom.xml'
-    publishJUnitResults: true
-    testResultsFiles: '**/surefire-reports/TEST-*.xml'
-    javaHomeOption: 'JDKVersion'
-    mavenVersionOption: 'Default'
-    mavenAuthenticateFeed: false
-    effectivePomSkip: false
-    sonarQubeRunAnalysis: false
