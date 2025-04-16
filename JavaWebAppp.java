2632774da1d1a9de61b9b26bcbb4970e17a90bf1
Aswinth123 <aswinth.k.cse26@psvpec.in>
2025-04-16 11:18:36 +0530

    Update JavaWebAppp.java for Azure Pipelines

--- JavaWebAppp.java
+++ JavaWebAppp.java
@@ -1,9 +1,7 @@
 import java.util.*;
   public class JavaWebAppp{
-    public static void main(String []args)
+    public static void main(String[] args)
     {
-      int a=10;
-      int b=10;
-      System.out.println("the Multiplication of a and b : "+a*b);
+      System.out.println("Welcome to the World");
     }
   }
