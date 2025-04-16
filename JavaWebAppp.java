103480878a752c366ce0687ebf4ac206117b2d25
Aswinth123 <aswinth.k.cse26@psvpec.in>
2025-04-16 10:49:15 +0530

    Create JavaWebAppp.java

---
+++ JavaWebAppp.java
@@ -0,0 +1,9 @@
+import java.util.*;
+  public class JavaWebAppp{
+    public static void main(String []args)
+    {
+      int a=10;
+      int b=10;
+      System.out.println("the Multiplication of a and b : "+a*b);
+    }
+  }
